08c651b8f994ab629ffec1d6203eec5c380fa8c1
José Passagnolo Neto <jose.passagnolo@forlogic.net>
2024-12-09 16:11:00 -0300

    criação do banco de dados, tabelas e adição de dados utilizando Migrations

---
+++ APIOperacaoCuriosidade/Migrations/20241209190449_PopulaPessoas.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace APIOperacaoCuriosidade.Migrations
+{
+    /// <inheritdoc />
+    public partial class PopulaPessoas : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder mb) {
+            mb.Sql("insert into Pessoas(" +
+                    "Nome, " +
+                    "Idade, " +
+                    "Email, " +
+                    "Endereco, " +
+                    "OutrasInformacoes, " +
+                    "Interesses, " +
+                    "Sentimentos, " +
+                    "Valores, " +
+                    "DataCadastro, " +
+                    "Status, " +
+                    "Deletado) " +
+                "Values(" +
+                    "'Son Goku', " +
+                    "35, " +
+                    "'goku@email.com', " +
+                    "'Distrito Leste 439', " +
+                    "'Seu poder é mais de 8 mil', " +
+                    "'Artes Marciais', " +
+                    "'sentimentos', " +
+                    "'valores', " +
+                    "getdate(), " +
+                    "'true', " +
+                    "'false')");
+            mb.Sql("insert into Pessoas(" +
+                    "Nome, " +
+                    "Idade, " +
+                    "Email, " +
+                    "Endereco, " +
+                    "OutrasInformacoes, " +
+                    "Interesses, " +
+                    "Sentimentos, " +
+                    "Valores, " +
+                    "DataCadastro, " +
+                    "Status, " +
+                    "Deletado) " +
+                "Values(" +
+                    "'Virgil Super Choque', " +
+                    "15, " +
+                    "'superchoque@email.com', " +
+                    "'Dakota', " +
+                    "'outras infos', " +
+                    "'interesses', " +
+                    "'sentimentos', " +
+                    "'valores', " +
+                    "getdate(), " +
+                    "'false', " +
+                    "'false')");
+            mb.Sql("insert into Pessoas(" +
+                    "Nome, " +
+                    "Idade, " +
+                    "Email, " +
+                    "Endereco, " +
+                    "OutrasInformacoes, " +
+                    "Interesses, " +
+                    "Sentimentos, " +
+                    "Valores, " +
+                    "DataCadastro, " +
+                    "Status, " +
+                    "Deletado) " +
+                "Values(" +
+                    "'Logan Wolverine', " +
+                    "100, " +
+                    "'wolverine@email.com', " +
+                    "'Alberta, Canadá', " +
+                    "'Inimigo do Dentes de Sabre', " +
+                    "'', " +
+                    "'', " +
+                    "'', " +
+                    "getdate(), " +
+                    "'true', " +
+                    "'false')");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder mb) {
+            mb.Sql("delete from Pessoas");
+        }
+    }
+}
---
+++ APIOperacaoCuriosidade/Migrations/20241209190653_PopulaUsuarios.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace APIOperacaoCuriosidade.Migrations
+{
+    /// <inheritdoc />
+    public partial class PopulaUsuarios : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder mb) {
+            mb.Sql("insert into Usuarios(" +
+                    "Nome, " +
+                    "Email, " +
+                    "Senha, " +
+                    "Deletado) " +
+                "Values(" +
+                    "'Admin', " +
+                    "'admin@email.com', " +
+                    "'admin', " +
+                    "'false')");
+            mb.Sql("insert into Usuarios(" +
+                    "Nome, " +
+                    "Email, " +
+                    "Senha, " +
+                    "Deletado) " +
+                "Values(" +
+                    "'Neto', " +
+                    "'neto@email.com', " +
+                    "'123', " +
+                    "'false')");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder mb) {
+            mb.Sql("delete from Usuarios");
+        }
+    }
+}
