22d8a6815cbbb236926c3545839bcf42d39ffac8
José Passagnolo Neto <jose.passagnolo@forlogic.net>
2024-12-10 08:46:36 -0300

    implementação inicial do padrão repository

---
+++ APIOperacaoCuriosidade/Repositories/IRepository.cs
@@ -0,0 +1,9 @@
+﻿namespace APIOperacaoCuriosidade.Repositories; 
+public interface IRepository<T> {
+    IEnumerable<T> BuscarTodos();
+    T BuscarPorId(int id);
+    T Criar(T entidade);
+    T Atualizar(T entidade);
+    T Deletar(int id);
+
+}
---
+++ APIOperacaoCuriosidade/Repositories/UsuarioRepository.cs
@@ -0,0 +1,51 @@
+﻿using APIOperacaoCuriosidade.Context;
+using APIOperacaoCuriosidade.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace APIOperacaoCuriosidade.Repositories;
+public class UsuarioRepository : IRepository<Usuario> {
+    private readonly ApiDbContext _context;
+
+    public UsuarioRepository(ApiDbContext context) {
+        _context = context;
+    }
+
+    public IEnumerable<Usuario> BuscarTodos() {
+        return _context.Usuarios.ToList();
+    }
+
+    public Usuario BuscarPorId(int id) {
+        return _context.Usuarios.FirstOrDefault(u => u.Id == id);
+    }
+
+    public Usuario Criar(Usuario usuario) {
+        if (usuario == null) {
+            throw new ArgumentNullException(nameof(usuario));
+        }
+
+        _context.Usuarios.Add(usuario);
+        _context.SaveChanges();
+        return usuario;
+    }
+
+    public Usuario Atualizar(Usuario usuario) {
+        if (usuario == null) {
+            throw new ArgumentNullException(nameof(usuario));
+        }
+
+        _context.Usuarios.Entry(usuario).State = EntityState.Modified;
+        _context.SaveChanges();
+        return usuario;
+    }
+
+    public Usuario Deletar(int id) {
+        var usuario = _context.Usuarios.Find(id);
+        if (usuario == null) {
+            throw new ArgumentNullException(nameof(usuario));
+        }
+
+        _context.Usuarios.Remove(usuario);
+        _context.SaveChanges();
+        return usuario;
+    }
+}
